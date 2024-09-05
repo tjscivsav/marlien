79814ea4182e9ebb5bcf80eb76ff6235f80abd8d
Usama Tahir <usamatahir8330@gmail.com>
2024-09-05 19:47:01 +0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [process.env.GA4_MEASUREMENT_ID],
+        trackingIds: [process.env.GTM_ID],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
         gtagConfig: {
