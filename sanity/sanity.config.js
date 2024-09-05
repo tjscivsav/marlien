8af50c0cb13dfb1dead9dcd910ad9d96ae7cef40
Usama Tahir <usamatahir8330@gmail.com>
2024-09-06 01:20:36 +0500

    Updated sanity deploy

--- sanity/sanity.config.js
+++ sanity/sanity.config.js
@@ -4,12 +4,13 @@ import {colorInput} from '@sanity/color-input'
 import {dashboardTool, projectInfoWidget} from '@sanity/dashboard'
 import {visionTool} from '@sanity/vision'
 import {defineConfig, definePlugin} from 'sanity'
-import {netlifyWidget} from 'sanity-plugin-dashboard-widget-netlify'
+// import {netlifyWidget} from 'sanity-plugin-dashboard-widget-netlify'
 import {media} from 'sanity-plugin-media'
 import {structureTool} from 'sanity/structure'
 import {MoveToWorkspace} from './actions'
 import {structure} from './deskStructure'
 import {schemaTypes} from './schemas'
+import {netlifyTool as netlifyDeploy} from 'sanity-plugin-netlify'
 
 const sharedConfig = definePlugin({
   name: 'default',
@@ -31,24 +32,25 @@ export default defineConfig([
 
     plugins: [
       sharedConfig(),
-      dashboardTool({
-        widgets: [
-          netlifyWidget({
-            title: 'My Netlify deploys',
-            sites: [
-              {
-                title: 'Marlien',
-                apiId: 'fb5cb1f6-2de5-44fe-9fcf-af6f4158e5c6',
-                buildHookId: '66994fcd52b08922dec88253',
-                name: 'marlien',
-                url: 'https://marlien.netlify.app/',
-                branch: 'main',
-              },
-            ],
-          }),
-          projectInfoWidget(),
-        ],
-      }),
+      netlifyDeploy(),
+      // dashboardTool({
+      //   widgets: [
+      //     // netlifyWidget({
+      //     //   title: 'My Netlify deploys',
+      //     //   sites: [
+      //     //     {
+      //     //       title: 'Marlien',
+      //     //       apiId: 'fb5cb1f6-2de5-44fe-9fcf-af6f4158e5c6',
+      //     //       buildHookId: '66994fcd52b08922dec88253',
+      //     //       name: 'marlien',
+      //     //       url: 'https://marlien.netlify.app/',
+      //     //       branch: 'main',
+      //     //     },
+      //     //   ],
+      //     // }),
+      //     projectInfoWidget(),
+      //   ],
+      // }),
     ],
     tools: [
       netlifyTool({
@@ -69,24 +71,25 @@ export default defineConfig([
 
     plugins: [
       sharedConfig(),
-      dashboardTool({
-        widgets: [
-          netlifyWidget({
-            title: 'My Netlify deploys',
-            sites: [
-              {
-                title: 'Marlien Dev',
-                apiId: 'fb5cb1f6-2de5-44fe-9fcf-af6f4158e5c6',
-                buildHookId: '66a10aa7e83d7c376d1b6ad6',
-                name: 'marlien-dev',
-                url: 'https://experiment--marlien.netlify.app',
-                branch: 'experiment',
-              },
-            ],
-          }),
-          projectInfoWidget(),
-        ],
-      }),
+      netlifyDeploy(),
+      // dashboardTool({
+      //   widgets: [
+      //     netlifyWidget({
+      //       title: 'My Netlify deploys',
+      //       sites: [
+      //         {
+      //           title: 'Marlien Dev',
+      //           apiId: 'fb5cb1f6-2de5-44fe-9fcf-af6f4158e5c6',
+      //           buildHookId: '66a10aa7e83d7c376d1b6ad6',
+      //           name: 'marlien-dev',
+      //           url: 'https://experiment--marlien.netlify.app',
+      //           branch: 'experiment',
+      //         },
+      //       ],
+      //     }),
+      //     projectInfoWidget(),
+      //   ],
+      // }),
     ],
 
     schema: {
